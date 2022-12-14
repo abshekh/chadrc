5e5cb3bd6b965878f28011d820d950c091b079da
Abhishek Singh <abhishek17513@gmail.com>
2022-12-14 14:11:01 +0530

    changes

--- init.lua
+++ init.lua
@@ -1,25 +1,5 @@
 require("custom.commands")
 require("custom.autocmds")
--- ❯ ln -s ~/.config/lazygit/config.yml ~/Library/Application\ Support/lazygit/config.yml
-
--- # if [ -n "$nvim_listen_address" ]; then #   alias nvim=nvr -cc tabnew --remote-wait +'set bufhidden=wipe'
--- # fi
--- # if [ -n "$NVIM_LISTEN_ADDRESS" ]; then
--- #     export VISUAL="nvr -cc tabnew --remote-wait +'set bufhidden=wipe'"
--- #     export EDITOR="nvr -cc tabnew --remote-wait +'set bufhidden=wipe'"
--- # else
--- #     export VISUAL="nvim"
--- #     export EDITOR="nvim"
--- # fi
--- vim.cmd [[
--- if has('nvim') && executable('nvr')
---   " let $GIT_EDITOR = "nvr -cc split --remote-wait +'set bufhidden=wipe'"
---   let $GIT_EDITOR = "nvr -cc tabe --remote-wait +'set bufhidden=wipe'"
--- endif
--- ]]
--- if vim.fn.executable("nvr") == 1 then
---   vim.env.GIT_EDITOR = "nvr -cc tabe --remote-wait +'set bufhidden=wipe'"
--- end
 
 -- :set diffopt+=vertical
 local set          = vim.opt
@@ -42,24 +22,3 @@ vim.cmd [[ autocmd User TelescopePreviewerLoaded setlocal wrap ]] -- wrap telesc
 vim.cmd [[ autocmd FileType * set formatoptions-=o ]] -- don't continue the comment
 
 vim.keymap.set("n", "<leader>gd", "<cmd>terminal lazygit<cr>")
-
-
--- local autocmd = vim.api.nvim_create_autocmd
--- autocmd("TermOpen", {
---   pattern = ".*lazygit",
---   command = function()
---     pcall(vim.keymap.del, 't', '<ESC>')
---   end,
--- })
-
-
--- vim.cmd [[ 
---   augroup neovim_lazygit
---       autocmd!
---       autocmd TermOpen *:lazygit startinsert
---       autocmd TermClose *:lazygit call feedkeys("i")
---   augroup END
--- ]]
-
--- run this to symlink the ftplugin
--- ln -s ~/.config/nvim/lua/custom/ftplugin/ ~/.config/nvim
